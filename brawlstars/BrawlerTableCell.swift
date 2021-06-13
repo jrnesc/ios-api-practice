23cc4f205f6118416749a000be730004531d4faf
Jaron Escoffery <jaronescoffery@192.168.1.140>
2021-06-14 00:56:40 +0100

    adding indexPath, still functionality issues w/ display

---
+++ brawlstars/BrawlerTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  BrawlerTableCell.swift
+//  brawlstars
+//
+//  Created by Jaron Escoffery on 10/06/2021.
+//
+
+import UIKit
+
+class BrawlerTableCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var gadget1: UILabel!
+    @IBOutlet weak var starPower1: UILabel!
+    
+    
+}
